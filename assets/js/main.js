f4eb5bc2ac3d73118c0909d47a602fe8435fbeba
mrinnnmoy <mrinmoy202000@gmail.com>
2022-08-31 21:18:43 +0530

    added script for functioning of nav bar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,23 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
 
+/* ========== MENU SHOW ========== */
+/* Validate if constatnt exist */
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/* MENU HIDDEN */
+/* Validate if constatnt exist */
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
 
